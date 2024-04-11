29284b908b29dab191d3d0451d08893dd3e18dc3
bibek190 <bibek9818917036@gmail.com>
2024-04-12 02:16:51 +1000

    Day 3 react-native

--- App.js
+++ App.js
@@ -1,38 +1,70 @@
-import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  ScrollView,
+  RefreshControl,
+} from "react-native";
 
-export default function App() {
-  const [number, setNumber] = useState(0);
-  const [count, setCount] = useState(0);
+const App = () => {
+  const [Items, setItems] = useState([
+    { key: 1, item: "Item 1" },
+    { key: 2, item: "Item 2" },
+    { key: 3, item: "Item 3" },
+    { key: 4, item: "Item 4" },
+    { key: 5, item: "Item 5" },
+    { key: 6, item: "Item 6" },
+    { key: 7, item: "Item 7" },
+    { key: 8, item: "Item 8" },
+    { key: 44, item: "Item 9" },
+    { key: 68, item: "Item 27" },
+    { key: 0, item: "Item 78" },
+  ]);
+  const [Refreshing, setRefreshing] = useState(false);
 
-  const onClickHandler = () => {
-    setNumber(number + 5);
-    setCount(count + 1);
-  };
-
-  const resetButton = () => {
-    setNumber(0);
-    setCount(0);
+  const onRefresh = () => {
+    setRefreshing(true);
+    setItems([...Items, { key: 69, item: "Item 69" }]);
+    setRefreshing(false);
   };
 
   return (
-    <View style={styles.container}>
-      <Text> {number}</Text>
-      <Button onPress={onClickHandler} title="Add" />
-      <Text>You have Clicked {count} times</Text>
-      <Button onPress={resetButton} title="Reset" />
-
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView
+      style={styles.body}
+      refreshControl={
+        <RefreshControl refreshing={false} onRefresh={onRefresh} />
+      }
+    >
+      {Items.map((object) => {
+        return (
+          <View style={styles.item} key={object.key}>
+            <Text style={styles.text}>{object.item}</Text>
+          </View>
+        );
+      })}
+    </ScrollView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  body: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    flexDirection: "column",
+    backgroundColor: "#ffffff",
+  },
+  item: {
+    margin: 20,
+    backgroundColor: "#4ae1fa",
     justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "#000000",
+    fontSize: 45,
+    fontStyle: "italic",
+    margin: 10,
   },
 });
+
+export default App;
