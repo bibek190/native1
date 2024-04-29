e58a6723350ecd2ddc08af415a2242ba7768f67c
bibek190 <bibek9818917036@gmail.com>
2024-04-30 01:08:43 +1000

    added function

--- App.js
+++ App.js
@@ -1,10 +1,45 @@
-import { StatusBar, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import {
+  Button,
+  FlatList,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [myGoal, setMyGoal] = useState("");
+  const [goalLists, setGoalLists] = useState([]);
+
+  const changingGoal = (changeInput) => {
+    setMyGoal(changeInput);
+  };
+
+  const onAdding = () => {
+    setGoalLists((prev) => [...prev, myGoal]);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Hello</Text>
+      <Text>Hello, Welcome to this App</Text>
+      <TextInput
+        style={styles.textInput}
+        placeholder="Write Your Goals"
+        value={myGoal}
+        onChangeText={changingGoal}
+      />
+      <Button title="Submit" color="green" onPress={onAdding} />
       <StatusBar style="auto" />
+      <View style={styles.goalLists}>
+        <FlatList
+          data={goalLists}
+          renderItem={(itemData) => {
+            return <Text>{itemData.item}</Text>;
+          }}
+        />
+      </View>
     </View>
   );
 }
@@ -15,5 +50,15 @@ const styles = StyleSheet.create({
     backgroundColor: "pink",
     alignItems: "center",
     justifyContent: "center",
+    paddingTop: 200,
+  },
+  textInput: {
+    padding: 16,
+    margin: 10,
+    minWidth: 200,
+    backgroundColor: "#fff",
+  },
+  goalLists: {
+    marginTop: 200,
   },
 });
